be3d3f7931f64ee49e10b7ab861baae49767969a
Valentin Bourreau <valentin.bourreau@gmail.com>
2021-11-28 20:06:54 +0100

    integrate errors pages

--- pages/500.tsx
+++ pages/500.tsx
@@ -1,13 +1,46 @@
 import React from 'react';
 import { NextSeo } from 'next-seo';
+import { useSpring, animated } from 'react-spring';
+import { Button, Container, Footer, Header, Title } from '@components';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import { useTranslation } from 'next-i18next';
+import Link from 'next/link';
 
 const Custom500 = (): JSX.Element => {
+    const container = useSpring({ to: { opacity: 1 }, from: { opacity: 0 } });
+    const { t } = useTranslation('common');
+
     return (
-        <>
-            <NextSeo noindex={true} />
-            <main className="template-error ep-flex-column">500</main>
-        </>
+        <animated.div style={container}>
+            <NextSeo title="valentinbourreau" noindex={true} />
+            <Container>
+                <Header />
+                <main
+                    style={{
+                        minHeight: 'calc(100vh - var(--gui-header-height) - var(--gui-footer-height))',
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        textAlign: 'center'
+                    }}>
+                    <Title level={1}>{t('ERRORS.500')}</Title>
+                    <Link href={'/'}>
+                        <a>
+                            <Button>{t('ERRORS.BACK')}</Button>
+                        </a>
+                    </Link>
+                </main>
+                <Footer />
+            </Container>
+        </animated.div>
     );
 };
+
+export const getStaticProps = async ({ locale }) => ({
+    props: {
+        ...(await serverSideTranslations(locale, ['common']))
+    }
+});
 
 export default Custom500;
